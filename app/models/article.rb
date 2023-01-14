0907cc27d490f8efd860892e731c1c196a13126e
Daniel Lee <danlee@hey.com>
2023-01-14 12:40:00 -0700

    wrote custom validation to make sure content length is over 270 chars

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,12 @@
 class Article < ApplicationRecord
   belongs_to :user
   has_rich_text :content
+
+  validate :content, :content_character_length
+
+  def content_character_length
+    if content.to_plain_text.size < 270
+      errors.add(:content, "content must be more than 270 characters")
+    end
+  end
 end
