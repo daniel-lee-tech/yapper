24bb8646a1b37e293b4611958d48dde5398ebd28
Daniel Lee <danlee@hey.com>
2023-01-14 12:04:40 -0700

    generated action text article

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,9 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "popper"
 import "bootstrap"
+import "trix"
+import "@rails/actiontext"
+
 
 document.addEventListener("DOMContentLoaded", function () {
     // This code is copied from Bootstrap's docs. See link below.
